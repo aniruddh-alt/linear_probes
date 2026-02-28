70bcb524c998600928a797af23839f55c0d1d7e0
Aniruddhan Ramesh <aniruddhanramesh@Aniruddhans-MacBook-Pro.local>
2026-02-28 15:46:44 -0800

    feat: add steering field to GenerateConfig

--- core/configs/generate_config.py
+++ core/configs/generate_config.py
@@ -7,6 +7,7 @@ from core.configs.base import BaseConfig
 from core.configs.params.generation_params import GenerationParams
 from core.configs.params.io_params import IOParams
 from core.configs.params.model_params import ModelParams
+from core.configs.params.steering_params import SteeringParams
 
 
 @dataclass
@@ -18,4 +19,5 @@ class GenerateConfig(BaseConfig):
     action: str = "generate"
     model: ModelParams = field(default_factory=ModelParams)
     generation: GenerationParams = field(default_factory=GenerationParams)
+    steering: SteeringParams = field(default_factory=SteeringParams)
     io: IOParams = field(default_factory=IOParams)
